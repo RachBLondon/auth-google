13a952fa001f97fa149bfefd83c6dd65a4641149
RACHELS MAC <rachelsmac@RACHELSs-Air.home>
2017-09-09 14:13:27 +0100

    sending user data in res, pulled from cookie

--- services/passport.js
+++ services/passport.js
@@ -6,12 +6,12 @@ const keys = require("./../config/keys");
 const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
-  done(err, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
   User.findById(id).then(user => {
-    done(err, user);
+    done(null, user);
   });
 });
 
@@ -26,10 +26,10 @@ passport.use(
       User.findOne({ googleID: profile.id }).then(existingUser => {
         if (existingUser) {
           //dont need to create a new record
-          done(err, existingUser);
+          done(null, existingUser);
         } else {
           new User({ googleID: profile.id }).save().then(user => {
-            done(err, user);
+            done(null, user);
           });
         }
       });
