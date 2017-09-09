a9b28e64da3803752f7ecfa5131769a26135c2ac
RACHELS MAC <rachelsmac@RACHELSs-Air.home>
2017-09-09 13:05:48 +0100

    serialize and deserialize user

--- services/passport.js
+++ services/passport.js
@@ -1,9 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const keys = require("./../config/keys");
 
-const User = mongoose.model('users');
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(err, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(err, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -13,8 +23,16 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-        new User({ googleID : profile.id }).save();
-        
+      User.findOne({ googleID: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //dont need to create a new record
+          done(err, existingUser);
+        } else {
+          new User({ googleID: profile.id }).save().then(user => {
+            done(err, user);
+          });
+        }
+      });
     }
   )
 );
